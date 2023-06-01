1b1d337c3cd810ec21c7d5bfcc2fded25fe62e89
rktdngud <dngud915@gmail.com>
2023-06-01 11:21:51 +0900

    commit

--- src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeGithubActionDeployApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PracticeGithubActionDeployApplication.class, args); 
+		SpringApplication.run(PracticeGithubActionDeployApplication.class, args);
+
 	}
 }
